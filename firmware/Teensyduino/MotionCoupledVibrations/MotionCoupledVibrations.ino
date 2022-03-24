bbe580b2c2552a94cd55a55f4f53105e81c4c307
derikon <dev@dasbuntwerk.de>
2022-03-24 21:41:42 +0100

    refactor: :recycle: clean code

--- firmware/Teensyduino/MotionCoupledVibrations/MotionCoupledVibrations.ino
+++ firmware/Teensyduino/MotionCoupledVibrations/MotionCoupledVibrations.ino
@@ -1,89 +1,174 @@
-// Updated code of binning algorithm to adjust for delays, debounce and latency
-// caused by high filter weights. Setting the max value based on the average of
-// the first 10 measurements and then mapping it to the full range (or using
-// that as the max and min in the mapping function).
-
-//=========== include libraries ===========
+//=========== system headers ===========
 #include <Arduino.h>
+
+//=========== library headers ===========
 #include <Audio.h>
 
-//=========== Audio Variables ===========
+namespace defaults {
+
+/**
+ * @brief Enumeration of waveforms that can be used for the signal generation.
+ * The values are copied from the Teensy Audio Library.
+ *
+ */
+enum class Waveform : short {
+  kSine = 0,
+  kSawtooth = 1,
+  kSquare = 2,
+  kTriangle = 3,
+  kArbitrary = 4,
+  kPulse = 5,
+  kSawtoothReverse = 6,
+  kSampleHold = 7,
+  kTriangleVariable = 8,
+  kBandlimitSawtooth = 9,
+  kBandlimitSawtoothReverse = 10,
+  kBandlimitSquare = 11,
+  kBandlimitPulse = 12
+};
+
+//=========== signal generator ===========
+static constexpr uint16_t kNumberOfBins = 50;
+static constexpr uint32_t kSignalDurationUs = 10000;
+static constexpr short kSignalWaveform = static_cast<short>(Waveform::kSine);
+static constexpr float kSignalFreqencyHz = 100.f;
+static constexpr float kSignalAmpPos = 0.56f;
+static constexpr float kSignalAmpNeg = 0.43f;
+
+//=========== sensor ===========
+static constexpr float kFilterWeight = 0.2;
+static constexpr uint8_t kSensorResolution = 10;
+static constexpr uint32_t kSensorMaxValue = (1U << kSensorResolution) - 1;
+static constexpr uint32_t kSensorMinValue = 0;
+static constexpr uint8_t kAnalogSensingPin = A0;
+
+//=========== serial ===========
+static constexpr int kBaudRate = 115200;
+}  // namespace defaults
+
+namespace {
+
+/**
+ * @brief a struct to hold the settings of the sensor.
+ *
+ */
+typedef struct {
+  float filter_weight = defaults::kFilterWeight;
+  uint8_t resolution = defaults::kSensorResolution;
+  uint32_t max_value = defaults::kSensorMaxValue;
+  uint32_t min_value = defaults::kSensorMinValue;
+} SensorSettings;
+
+/**
+ * @brief a struct to hold the settings for the signal generator.
+ *
+ */
+typedef struct {
+  uint16_t number_of_bins = defaults::kNumberOfBins;
+  uint32_t duration_us = defaults::kSignalDurationUs;
+  short waveform = defaults::kSignalWaveform;
+  float frequency_hz = defaults::kSignalFreqencyHz;
+  float amp_pos = defaults::kSignalAmpPos;
+  float amp_neg = defaults::kSignalAmpNeg;
+} SignalGeneratorSettings;
+
+//=========== settings instances ===========
+// These instances are used to access the settings in the main code.
+SensorSettings sensor_settings;
+SignalGeneratorSettings signal_generator_settings;
+
+//=========== audio variables ===========
 AudioSynthWaveform signal;
-AudioOutputPT8211 toHaptuator;
-AudioConnection patchCord1(signal, 0, toHaptuator, 0);
-AudioConnection patchCord2(signal, 0, toHaptuator, 1);
+AudioOutputPT8211 to_haptuator;
+AudioConnection patchCord1(signal, 0, to_haptuator, 0);
+AudioConnection patchCord2(signal, 0, to_haptuator, 1);
 
-//=========== Other Variables ===========
-#define MESSAGESIZE 4
-//{#bins, freq, amp-pos, amp-neg}
-float receivedMessage[MESSAGESIZE] = {50, 100, 0.56, 0.43};
-unsigned long vibrationDuration = 10;
-unsigned long vibrationStartTime;
+//=========== sensor variables ===========
+float filtered_sensor_value = 0.f;
 
-//=========== Sensor Variables ===========
-#define sensingPin A0
-float filtered_val = 0.0;
-float filter_weight = 0.2;
+//=========== control flow variables ===========
+elapsedMicros pulse_time_us = 0;
+bool is_vibrating = false;
+uint16_t last_bin_id = 0;
+uint16_t new_pulse_id = 0;
+uint16_t current_pulse_id = 0;
 
-const uint8_t sensorResolution = 10;  // 10 bit resolution
-const int sensorMaxValue = (1 << sensorResolution) - 1;
-const int sensorMinValue = 0;
-int led = 13;
-int count = 0;
+//=========== helper functions ===========
+// These functions were extracted to simplify the control flow and will be
+// inlined by the compiler.
+inline void StartPulse() __attribute__((always_inline));
+inline void StopPulse() __attribute__((always_inline));
 
-//=========== Descriptors ===========
-bool slope_is_positive = true;
-int direction = 1;
-int lastBinNumber = 0;
-bool isVibrating = false;  // Checks if the haptuator is vibrating
-bool newTrigger = true;    // New trigger to initiate the one bin
-int newPulseID = 0;        // ID of new pulse
-int currentPulseID = 0;    // ID of ongoing pulse
-// its allways the smaller of the two option,
-// so when going from 4 to 5, its 5, when going from 7 to 6 its 6
+/**
+ * @brief start a pulse by setting the amplitude of the signal to a predefined
+ * value
+ *
+ */
+void StartPulse() {
+  pulse_time_us = 0;
+  signal.amplitude(signal_generator_settings.amp_pos);
+  signal.begin(signal_generator_settings.waveform);
+  is_vibrating = true;
+}
 
-// do we need a current bin-number?
-//======================================
+/**
+ * @brief stop the pulse by setting the amplitude of the signal to zero
+ *
+ */
+void StopPulse() {
+  signal.amplitude(0.f);
+  is_vibrating = false;
+}
+}  // namespace
 
 void setup() {
+  // initialize the serial communication
+  while (!Serial && millis() < 5000)
+    ;
+  Serial.begin(defaults::kBaudRate);
+
+  // initialize the audio processing
   AudioMemory(20);
   delay(50);  // time for DAC voltage stable
-  // Serial.begin(115200);
-  pinMode(sensingPin, INPUT);
-  analogReadRes(sensorResolution);  // Uncomment this for button press.
+  signal.begin(signal_generator_settings.waveform);
+  signal.frequency(signal_generator_settings.frequency_hz);
 
-  signal.begin(WAVEFORM_SINE);
-  signal.frequency(receivedMessage[1]);
-  pinMode(led, OUTPUT);  // Used for Debugging and debounce condition
+  // initialize the sensor
+  pinMode(defaults::kAnalogSensingPin, INPUT);
+  analogReadRes(sensor_settings.resolution);
 }
 
 void loop() {
-  auto rheo_read = analogRead(sensingPin);
-  filtered_val =
-      (1.0 - filter_weight) * filtered_val + filter_weight * rheo_read;
-  // Function for the type of mapping. (Currently used is Linear)
-  int currentBinNumber =
-      map(filtered_val, sensorMinValue, sensorMaxValue, 0, receivedMessage[0]);
+  // read the sensor value and filter it
+  auto sensor_value = analogRead(defaults::kAnalogSensingPin);
+  filtered_sensor_value =
+      (1.f - sensor_settings.filter_weight) * filtered_sensor_value +
+      sensor_settings.filter_weight * sensor_value;
 
-  //=========== Logic ===========
-  // Trigger if pointer moves into a new bin.
-  if (currentBinNumber != lastBinNumber) {
+  // calculate the bin id depending on the filtered sensor value
+  // (currently linear mapping)
+  uint16_t mapped_bin_id = map(filtered_sensor_value, sensor_settings.min_value,
+                               sensor_settings.max_value, 0,
+                               signal_generator_settings.number_of_bins);
+
+  if (mapped_bin_id != last_bin_id) {
+    //! bin CHANGED
+
     // ******* #1 Assign a pulse number *************************
-    // check for the pulse ID
-    if (currentBinNumber > lastBinNumber) {
-      newPulseID = currentBinNumber;
+    if (mapped_bin_id > last_bin_id) {
+      new_pulse_id = mapped_bin_id;
     } else {
-      newPulseID = lastBinNumber;
+      new_pulse_id = mapped_bin_id;
     }
 
     // ******* #2 Check if Pulse is already vibrating *************************
     // if its not vibrating, start a new pulse with a new ID
-    if (!isVibrating) {
-      // currentPulseID = newPulseID;
+    if (!is_vibrating) {
+      // current_pulse_id = new_pulse_id;
     }
 
     // ******* #3 Check if we have a new Pulse ID *************************
-    if (newPulseID == currentPulseID) {
+    if (new_pulse_id == current_pulse_id) {
       // don't do anything if its already vibrating
       // with an existing pulse
       // don't retrigger the same pulse!
@@ -91,20 +176,16 @@ void loop() {
     } else {
       //! currently same pulses cannot retrigger at all
       //! this is maybe too strict
-      currentPulseID = newPulseID;
-      vibrationStartTime = millis();
-      signal.amplitude(
-          receivedMessage[2]);  //<-- maybe give these explicit variables
-      isVibrating = true;
+      current_pulse_id = new_pulse_id;
+      StartPulse();
     }
-    lastBinNumber = currentBinNumber;
+
+    last_bin_id = mapped_bin_id;
   }
 
-  if (isVibrating) {
-    // if the duration is over set volume to zero
-    if (millis() >= vibrationStartTime + vibrationDuration) {
-      signal.amplitude(0.f);
-      isVibrating = false;
+  if (is_vibrating) {
+    if (pulse_time_us >= signal_generator_settings.duration_us) {
+      StopPulse();
     }
   }
 }
